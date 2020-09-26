9f744b6f995b07d5ee53efd856d50a5c3a684bfb
Darkweak <sylvain@les-tilleuls.coop>
2020-09-26 21:31:11 +0200

    Add capabilty to delegate to user additional JVM arguments before launch

--- src/main/java/fr/trxyy/alternative/alternative_api/build/GameRunner.java
+++ src/main/java/fr/trxyy/alternative/alternative_api/build/GameRunner.java
@@ -32,7 +32,7 @@ public class GameRunner {
 
 	private GameEngine engine;
 	private Session session;
-	
+
 	public GameRunner(GameEngine gameEngine, Session account) {
 		this.engine = gameEngine;
 		this.session = account;
@@ -51,10 +51,14 @@ public class GameRunner {
 			});
 		}
 	}
-	  
-    public void launch() throws Exception
+
+	public void launch() {
+		this.launch(new String[]);
+	}
+
+    public void launch(String[] parameters) throws Exception
     {
-    	ArrayList<String> commands = getLaunchCommand();
+    	ArrayList<String> commands = getLaunchCommand(parameters);
         ProcessBuilder processBuilder = new ProcessBuilder(commands);
         processBuilder.redirectInput(Redirect.INHERIT);
         processBuilder.redirectOutput(Redirect.INHERIT);
@@ -91,7 +95,7 @@ public class GameRunner {
 			throw new Exception("Cannot launch !", e);
 		}
 	}
-    
+
 	public void openLink(String urlString) {
 		try {
 			Desktop.getDesktop().browse(new URL(urlString).toURI());
@@ -99,25 +103,27 @@ public class GameRunner {
 			e.printStackTrace();
 		}
 	}
-    
-	private ArrayList<String> getLaunchCommand() {
+
+	private ArrayList<String> getLaunchCommand(String[] additionalCommands) {
 		ArrayList<String> commands = new ArrayList<String>();
 		OperatingSystem os = OperatingSystem.getCurrentPlatform();
         commands.add(OperatingSystem.getJavaPath());
         commands.add("-XX:-UseAdaptiveSizePolicy");
-        commands.add("-XX:+UseConcMarkSweepGC");
-		
+		for (additionalCommand: additionalCommands) {
+			commands.add(additionalCommand);
+		}
+
 		if (os.equals(OperatingSystem.OSX)) {
 			commands.add("-Xdock:name=Minecraft");
 			commands.add("-Xdock:icon=" + engine.getGameFolder().getAssetsDir() + "icons/minecraft.icns");
-			commands.add("-XX:+CMSIncrementalMode");
 		} else if (os.equals(OperatingSystem.WINDOWS)) {
+			commands.add("-XX:+UseConcMarkSweepGC");
 			commands.add("-XX:HeapDumpPath=MojangTricksIntelDriversForPerformance_javaw.exe_minecraft.exe.heapdump");
 		}
 		commands.add("-Djava.library.path=" + engine.getGameFolder().getNativesDir().getAbsolutePath());
 		commands.add("-Dfml.ignoreInvalidMinecraftCertificates=true");
 		commands.add("-Dfml.ignorePatchDiscrepancies=true");
-		
+
 		boolean is32Bit = "32".equals(System.getProperty("sun.arch.data.model"));
 		String defaultArgument = is32Bit ? "-Xmx512M -Xmn128M" : "-Xmx1G -Xmn128M";
 		if (engine.getGameMemory() != null) {
@@ -126,11 +132,11 @@ public class GameRunner {
 		String str[] = defaultArgument.split(" ");
 		List<String> args = Arrays.asList(str);
 		commands.addAll(args);
-		
+
 		commands.add("-cp");
 		commands.add("\"" + GameUtils.constructClasspath(engine) + "\"");
 		commands.add(engine.getGameStyle().getMainClass());
-		
+
 		/** ----- Minecraft Arguments ----- */
 		if (engine.getMinecraftVersion().getMinecraftArguments() != null) {
 	        final String[] argsD = getArgumentsOlder();
@@ -151,29 +157,29 @@ public class GameRunner {
 			List<String> newerList = Arrays.asList(strcs);
 			commands.addAll(newerList);
 		}
-		
+
 		/** ----- Addons arguments ----- */
 		if (engine.getGameArguments() != null) {
 			commands.addAll(engine.getGameArguments().getArguments());
 		}
-		
+
 		/** ----- Size of window ----- */
 		if (engine.getGameSize() != null) {
 			commands.add("--width=" + engine.getGameSize().getWidth());
 			commands.add("--height=" + engine.getGameSize().getHeight());
 		}
-		
+
 		/** ----- Change properties of Forge (1.13+) ----- */
 		if (engine.getGameStyle().getSpecificsArguments() != null) {
 			commands.addAll(getForgeArguments());
 		}
-		
+
 		/** ----- Direct connect to a server if required. ----- */
 		if (engine.getGameConnect() != null) {
 			commands.add("--server=" + engine.getGameConnect().getIp());
 			commands.add("--port=" + engine.getGameConnect().getPort());
 		}
-		
+
 		/** ----- Tweak Class if required ----- */
 		if (engine.getGameStyle().equals(GameStyle.FORGE_1_7_10_OLD) || engine.getGameStyle().equals(GameStyle.FORGE_1_8_TO_1_12_2) || engine.getGameStyle().equals(GameStyle.OPTIFINE) || engine.getGameStyle().equals(GameStyle.ALTERNATIVE)) {
 			commands.add("--tweakClass");
@@ -181,7 +187,7 @@ public class GameRunner {
 		}
 		return commands;
 	}
-	
+
 	private List<String> getForgeArguments() {
 		String specfs = engine.getGameStyle().getSpecificsArguments();
 		specfs = specfs.replace("${launch_target_fml}", GameForge.getLaunchTarget())
@@ -214,7 +220,7 @@ public class GameRunner {
 
 		return split;
 	}
-	
+
 	private String[] getArgumentsNewer(List<Argument> args) {
 		final Map<String, String> map = new HashMap<String, String>();
 		final StrSubstitutor substitutor = new StrSubstitutor(map);
@@ -238,7 +244,7 @@ public class GameRunner {
 
 		return split;
 	}
-	
+
 	private void unpackNatives() {
 		try {
 			FileUtil.unpackNatives(engine.getGameFolder().getNativesDir(), engine);
@@ -248,7 +254,7 @@ public class GameRunner {
 			return;
 		}
 	}
-	
+
 	private void deleteFakeNatives() {
 		try {
 			FileUtil.deleteFakeNatives(engine.getGameFolder().getNativesDir(), engine);
@@ -258,7 +264,7 @@ public class GameRunner {
 			return;
 		}
 	}
-	
+
 	public static List<String> hideAccessToken(String[] arguments) {
         final ArrayList<String> output = new ArrayList<String>();
         for (int i = 0; i < arguments.length; i++) {
